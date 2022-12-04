f1c3d1842f852dc0a19fd6d51bb440988971f571
“PriyankaShinde” <pkshinde125@gmail.com>
2022-12-04 22:42:21 +0530

    ADDED : Code to find maximum string among three

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,30 @@ public class Main {
         System.out.println("Welcome to generics....");
         Main main = new Main();
         Scanner sc = new Scanner(System.in);
-       System.out.println("Enter three integer numbers");
+        System.out.println("Enter three integer numbers");
         Integer intNum1 = sc.nextInt();
         Integer intNum2 = sc.nextInt();
         Integer intNum3 = sc.nextInt();
-        System.out.println("Maximum number is : " + main.maxInt(intNum1,intNum2,intNum3));
+        System.out.println("Maximum number is : " + main.maxInt(intNum1, intNum2, intNum3));
         System.out.println("Enter three float numbers : ");
-        Float floatNum1= sc.nextFloat();
-        Float floatNum2= sc.nextFloat();
-        Float floatNum3= sc.nextFloat();
-        System.out.println("Maximum number is : " + main.maxFloat(floatNum1,floatNum2,floatNum3));
+        Float floatNum1 = sc.nextFloat();
+        Float floatNum2 = sc.nextFloat();
+        Float floatNum3 = sc.nextFloat();
+        System.out.println("Maximum number is : " + main.maxFloat(floatNum1, floatNum2, floatNum3));
+        System.out.println("Enter three Strings : ");
+        String string1 = sc.next();
+        String string2 = sc.next();
+        String string3 = sc.next();
+        System.out.println("Maximum number is : " + main.maxString(string1, string2, string3));
+    }
+
+    private String maxString(String string1, String string2, String string3) {
+        String max = string1;
+        if (string2.compareTo(string1) > 0)
+            max = string2;
+        if (string3.compareTo(string2) > 0)
+            max = string3;
+        return max;
     }
 
     private Float maxFloat(Float floatNum1, Float floatNum2, Float floatNum3) {
