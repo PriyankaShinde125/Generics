53618df6e5a754e2c2968cd8c5e896665b1a8076
“PriyankaShinde” <pkshinde125@gmail.com>
2022-12-04 22:53:56 +0530

    REFACTORED : methods to find maximum convert to generic type method

--- src/Main.java
+++ src/Main.java
@@ -9,43 +9,25 @@ public class Main {
         Integer intNum1 = sc.nextInt();
         Integer intNum2 = sc.nextInt();
         Integer intNum3 = sc.nextInt();
-        System.out.println("Maximum number is : " + main.maxInt(intNum1, intNum2, intNum3));
+        System.out.println("Maximum number is : " + main.max(intNum1, intNum2, intNum3));
         System.out.println("Enter three float numbers : ");
         Float floatNum1 = sc.nextFloat();
         Float floatNum2 = sc.nextFloat();
         Float floatNum3 = sc.nextFloat();
-        System.out.println("Maximum number is : " + main.maxFloat(floatNum1, floatNum2, floatNum3));
+        System.out.println("Maximum number is : " + main.max(floatNum1, floatNum2, floatNum3));
         System.out.println("Enter three Strings : ");
         String string1 = sc.next();
         String string2 = sc.next();
         String string3 = sc.next();
-        System.out.println("Maximum number is : " + main.maxString(string1, string2, string3));
+        System.out.println("Maximum number is : " + main.max(string1, string2, string3));
     }
 
-    private String maxString(String string1, String string2, String string3) {
-        String max = string1;
-        if (string2.compareTo(string1) > 0)
-            max = string2;
-        if (string3.compareTo(string2) > 0)
-            max = string3;
-        return max;
-    }
-
-    private Float maxFloat(Float floatNum1, Float floatNum2, Float floatNum3) {
-        Float max = floatNum1;
-        if (floatNum2.compareTo(floatNum1) > 0)
-            max = floatNum2;
-        if (floatNum3.compareTo(floatNum2) > 0)
-            max = floatNum3;
-        return max;
-    }
-
-    private Integer maxInt(Integer intNum1, Integer intNum2, Integer intNum3) {
-        Integer max = intNum1;
-        if (intNum2.compareTo(intNum1) > 0)
-            max = intNum2;
-        if (intNum3.compareTo(intNum2) > 0)
-            max = intNum3;
+    private <T extends Comparable<T>> T max(T val1, T val2, T val3) {
+        T max = val1;
+        if (val2.compareTo(val1) > 0)
+            max = val2;
+        if (val3.compareTo(val2) > 0)
+            max = val3;
         return max;
     }
 }
