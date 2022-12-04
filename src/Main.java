8800736a4b47e2a906ed4fb19f945a7757abb8ce
“PriyankaShinde” <pkshinde125@gmail.com>
2022-12-04 22:35:57 +0530

    ADDED : Code to find maximum among three float

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,33 @@ public class Main {
         System.out.println("Welcome to generics....");
         Main main = new Main();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter three numbers");
+       System.out.println("Enter three integer numbers");
         Integer intNum1 = sc.nextInt();
         Integer intNum2 = sc.nextInt();
         Integer intNum3 = sc.nextInt();
-        System.out.println("Maximum number is : " + main.max(intNum1,intNum2,intNum3));
+        System.out.println("Maximum number is : " + main.maxInt(intNum1,intNum2,intNum3));
+        System.out.println("Enter three float numbers : ");
+        Float floatNum1= sc.nextFloat();
+        Float floatNum2= sc.nextFloat();
+        Float floatNum3= sc.nextFloat();
+        System.out.println("Maximum number is : " + main.maxFloat(floatNum1,floatNum2,floatNum3));
     }
 
-    private Integer max(Integer num1, Integer num2, Integer num3) {
-        Integer max = num1;
-        if (num2.compareTo(num1) > 0)
-            max = num2;
-        if (num3.compareTo(num2) > 0)
-            max = num3;
+    private Float maxFloat(Float floatNum1, Float floatNum2, Float floatNum3) {
+        Float max = floatNum1;
+        if (floatNum2.compareTo(floatNum1) > 0)
+            max = floatNum2;
+        if (floatNum3.compareTo(floatNum2) > 0)
+            max = floatNum3;
+        return max;
+    }
+
+    private Integer maxInt(Integer intNum1, Integer intNum2, Integer intNum3) {
+        Integer max = intNum1;
+        if (intNum2.compareTo(intNum1) > 0)
+            max = intNum2;
+        if (intNum3.compareTo(intNum2) > 0)
+            max = intNum3;
         return max;
     }
 }
