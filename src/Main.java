23316c6a77f594194e81182989d56f6030b8b882
“PriyankaShinde” <pkshinde125@gmail.com>
2022-12-04 22:22:51 +0530

    ADDED : Code to find maximum of three integers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to generics....");
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter three numbers");
+        Integer intNum1 = sc.nextInt();
+        Integer intNum2 = sc.nextInt();
+        Integer intNum3 = sc.nextInt();
+        System.out.println("Maximum number is : " + main.max(intNum1,intNum2,intNum3));
+    }
+
+    private Integer max(Integer num1, Integer num2, Integer num3) {
+        Integer max = num1;
+        if (num2.compareTo(num1) > 0)
+            max = num2;
+        if (num3.compareTo(num2) > 0)
+            max = num3;
+        return max;
     }
 }
